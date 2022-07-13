06ced56d4c594ee7642e86d36c89c1e8400de945
Raziel <chokokokyunokibou@gmail.com>
2022-07-12 22:05:43 -0400

    GET, POST, PUT de Clientes rudimentario y revisión parcial de integridad de datos de JSON de entrada en POST

--- app/app.py
+++ app/app.py
@@ -13,11 +13,32 @@ def get_clientes():
     cc = conexion_clientes.ConexionCliente()
     tupla_clientes = cc.listar_clientes()
     clientes = []
-    for tc in tupla_clientes:
-        telefono_con_simbolo = "+" + tc[3]
-        diccionario_cliente = {"cedula":tc[0],"name":tc[1],"email":tc[2],"whatsapp":telefono_con_simbolo}
-        clientes.append(diccionario_cliente)
+    if tupla_clientes != None:
+        for tc in tupla_clientes:
+            telefono_con_simbolo = "+" + tc[3]
+            diccionario_cliente = {"cedula":tc[0],"name":tc[1],"email":tc[2],"whatsapp":telefono_con_simbolo}
+            clientes.append(diccionario_cliente)
     return jsonify(clientes),200
+
+@app.route("/customers", methods = ["POST"])
+def post_cliente():
+    dict_cliente = request.get_json()
+    if {'cedula','cliente','email','whatsapp'} <= set(dict_cliente):
+        cc = conexion_clientes.ConexionCliente()
+        whatsapp = dict_cliente["whatsapp"][1:]
+        cc.insertar_cliente(dict_cliente["cedula"],dict_cliente["name"],dict_cliente["email"],whatsapp)
+        return '',201
+    else
+        print("Error en JSON de entrada")
+        abort(400)
+
+@app.route("/customers/<cedula>",methods = ["PUT"])
+def put_cliente(cedula):
+    dict_cliente = request.get_json()
+    cc = conexion_clientes.ConexionCliente()
+    whatsapp = dict_cliente["whatsapp"][1:]
+    cc.modificar_cliente(cedula,dict_cliente["name"],whatsapp,dict_cliente["email"])
+    return '', 200
 
 def pagina_no_encontrada(error):
     return "<h1>Error 404<h2><h2>Página no encontrada</h2>"
